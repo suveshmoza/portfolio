0f19d0363276ee64afdfe7db62517921ccfb2c78
Suvesh Moza <hellosuvesh@gmail.com>
2024-02-12 02:27:04 +0530

    add: blogs page

---
+++ app/blogs/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next';
+
+type Props = {};
+
+export const metadata: Metadata = {
+	title: "Suvesh Blog's",
+	description: 'Blogs Wriiten by Suvesh Moza.',
+};
+
+export default function Blogs({}: Props) {
+	return <p>Coming Soon...</p>;
+}
